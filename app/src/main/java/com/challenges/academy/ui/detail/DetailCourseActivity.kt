07dcdc539c28e536575a0583c552eea19bea076c
Gustiar31 <guztiar.profile@gmail.com>
2021-02-07 21:20:24 +0700

    commit Layouting

--- app/src/main/java/com/challenges/academy/ui/detail/DetailCourseActivity.kt
+++ app/src/main/java/com/challenges/academy/ui/detail/DetailCourseActivity.kt
@@ -1,12 +1,15 @@
 package com.challenges.academy.ui.detail
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.challenges.academy.R
+import com.challenges.academy.databinding.ActivityDetailCourseBinding
+import com.challenges.academy.databinding.ContentDetailCourseBinding
 
 class DetailCourseActivity : AppCompatActivity() {
+
+    private lateinit var detailContentBinding: ContentDetailCourseBinding
+
 
     companion object {
         const val EXTRA_COURSE = "extra_course"
@@ -14,14 +17,13 @@ class DetailCourseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_course)
+
+        val activityDetailCourseBinding = ActivityDetailCourseBinding.inflate(layoutInflater)
+        detailContentBinding = activityDetailCourseBinding.detailContent
+
+        setContentView(activityDetailCourseBinding.root)
         setSupportActionBar(findViewById(R.id.toolbar))
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 }
